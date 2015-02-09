d28df32ef9468823e87ae4f0445967c57d4ada4f
Jonathan Powers <bigblackrobot@gmail.com>
2015-02-09 16:52:34 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function myDopeFunction(a, b) {
   var x = myAwesomeFunction(4, 3);
   var x = myReallyAwesomeFunction(4, 3);
-  var a = '<a href="javascript:alert(\'I am a bookmarklet\');" >Drag me to your Toolbar</a>';
+  var c = '<a href="javascript:alert(\'I am a bookmarklet\');" >Drag me to your Toolbar</a>';
   return a * b;
 }
