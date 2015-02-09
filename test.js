31390752015a5fd86c498315cd9f3df60a39afb7
Jonathan Powers <bigblackrobot@gmail.com>
2015-02-09 17:31:52 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function myDopeFunction(a, b) {
   var x = myAwesomeFunction(4, 3);
   var x = myReallyAwesomeFunction(4, 3);
-  var c = '<a href="javascript:alert(\'I am a bookmarklet\');" >Drag me to your Toolbar</a>';
+  var test = "javascript:displayLink('" + foo.bar + "')";
   return a * b;
 }
