9622d393db6088ecb6e345e13924fc111a595d7b
Jonathan Powers <bigblackrobot@gmail.com>
2015-02-10 11:09:43 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,16 @@
 function myDopeFunction(a, b) {
   var x = myAwesomeFunction(4, 3);
-  var x = myReallyAwesomeFunction(4, 3);
-  var test = "javascript:displayLink('" + foo.bar + "')";
+  var y = myReallyAwesomeFunction(4, 3);
+  
+  var nums = [];
+
+  for (var i = 0; i < 10; i++) {
+    nums[i] = function (j) {
+      return i + j;
+    };
+  }
+  
+  nums[0](2); // Prints 12 instead of 2
+  
   return a * b;
 }
